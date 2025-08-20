f191fbaa318de641dd828d189d5a4d6415d919c7
AlysieLuong <alysieluongg@gmail.com>
2025-08-20 12:34:30 +1000

    made-header

---
+++ app/header.tsx
@@ -0,0 +1,16 @@
+import NavBar from './navbar';
+import styles from './header.module.css';
+
+export default function Header() {
+    return (
+        <>
+        <header>
+            <div>
+                <h1> CSE3CWA Assignment 1</h1>
+                <h3> 21612368</h3>
+            </div>
+                <NavBar></NavBar>
+        </header>
+        </>
+    )
+}
