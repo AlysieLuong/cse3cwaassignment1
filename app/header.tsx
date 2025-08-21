260a518919cd7d66a60069a9e7af3d8e4c91926c
AlysieLuong <alysieluongg@gmail.com>
2025-08-22 01:08:22 +1000

    initial-appearance

--- app/header.tsx
+++ app/header.tsx
@@ -1,16 +1,20 @@
 import NavBar from './navbar';
 import styles from './header.module.css';
 
-export default function Header() {
+type HeaderProps = {
+    children? :React.ReactNode;
+};
+
+export default function Header( {children}: HeaderProps) {
     return (
-        <>
-        <header>
-            <div>
-                <h1> CSE3CWA Assignment 1</h1>
-                <h3> 21612368</h3>
+        <header className={styles.header}>
+            <div className={styles.titleContainer}>
+                <h1 className={styles.mainTitle}> CSE3CWA Assignment 1</h1>
+                <h3 className={styles.subTitle}> 21612368</h3>
             </div>
-                <NavBar></NavBar>
+                <NavBar>
+                    {children}
+                </NavBar>
         </header>
-        </>
     )
 }
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import Link from 'next/link';
+import Styles from './navbar.module.css';
 
-export default function NavBar() {
+type NavBarProps = {
+    children?: React.ReactNode;
+};
+
+export default function NavBar({ children }: NavBarProps) {
   return (
-      <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+    <nav className={Styles.nav}>
+      <Link href="/" className={Styles.link}>Home</Link>
+      <Link href="/about" className={Styles.link}>About</Link>
+      <Link href="/coding-resources" className={Styles.link}>Coding Resources</Link>
+      <Link href="/escape-room" className={Styles.link}>Escape Room</Link>
+      {children}
     </nav>
   );
 }
